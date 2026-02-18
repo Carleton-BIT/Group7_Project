da2c2f487c7603ce08b02a13a0fd67e7ccf5a54f
power <powerliam05@icloud.com>
2026-02-18 14:09:45 -0500

    Did the bare minimum here, nothing crazy to see at all, going to eat supper, I should be back at this later

--- Advising.sql
+++ Advising.sql
@@ -1,10 +1,12 @@
 -- This is basically created as a back-up to make sure that if anything goes wrong in the console, I can have a back-up to fall back on.
 
-CREATE TABLE academic_services (
-    service_id INT PRIMARY KEY,
+CREATE TABLE academic_services
+(
+    service_id       INT PRIMARY KEY,
     service_category VARCHAR(100),
-    service_name VARCHAR(100),
-    description VARCHAR(400),
-    contact_email VARCHAR(100),
-    location VARCHAR(100),
-    operating_hours VARCHAR(100),
+    service_name     VARCHAR(100),
+    description      VARCHAR(400),
+    contact_email    VARCHAR(100),
+    location         VARCHAR(100),
+    operating_hours  VARCHAR(100),
+)
